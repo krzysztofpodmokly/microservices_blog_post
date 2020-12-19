24bb0f510f2f80987363eb647d33026d28bcf4ab
Krzysztof <krzysztof.podmokly@gmail.com>
2020-12-19 22:02:38 +0100

    Adding basic event bus

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,7 +14,7 @@ app.get('/posts/:id/comments', (req, res) => {
     res.status(200).send(commentsByPostId[req.params.id] || []);
 })
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const { id } = req.params;
     const { content } = req.body;
@@ -24,7 +25,20 @@ app.post('/posts/:id/comments', (req, res) => {
 
     commentsByPostId[id] = comments;
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId, content, postId: id
+        }
+    })
+
     res.status(201).send(comments)
+});
+
+app.post('/events', (req, res) => {
+    console.log('Received Event', req.body.type);
+
+    res.send({});
 });
 
 app.listen(4001, () => {
