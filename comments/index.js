52bbb0eb0b8ae31c3bf3aa62c3ef7e7b98978acd
Krzysztof <krzysztof.podmokly@gmail.com>
2020-12-20 19:00:14 +0100

    Adding moderation service - orange

--- comments/index.js
+++ comments/index.js
@@ -21,22 +21,46 @@ app.post('/posts/:id/comments', async (req, res) => {
 
     const comments = commentsByPostId[id] || [];
 
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status: 'pending' });
 
     commentsByPostId[id] = comments;
 
     await axios.post('http://localhost:4005/events', {
         type: 'CommentCreated',
         data: {
-            id: commentId, content, postId: id
+            id: commentId, content, postId: id, status: 'pending'
         }
     })
 
     res.status(201).send(comments)
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Received Event', req.body.type);
+
+    const { type, data } = req.body;
+
+    if (type === 'CommentModerated') {
+        const { postId, id, status, content } = data;
+
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+
+        comment.status = status;
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                status,
+                postId,
+                content
+            }
+        })
+    }
 
     res.send({});
 });
